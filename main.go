1c5d30148702068426ac8c27fa48497323561bc5
Taki Do <ferrusi.enzo06@gmail.com>
2025-03-20 12:06:01 +0100

    Login

--- main.go
+++ main.go
@@ -9,12 +9,7 @@ import (
 
 func main() {
 	Database.ConnectDB()
-<<<<<<< HEAD
-	var err = Database.RegisterUser("Takido", "test")
-	print(err)
-=======
 	Database.RegisterUser("Takido", "test")
->>>>>>> 3556f0aece8406937bfaf1dbff4d4b0c0a91241f
 	http.HandleFunc("/", PageHandlers.LoginPage)
 	http.HandleFunc("/login", API.Login)
 	http.ListenAndServe(":80", nil)
