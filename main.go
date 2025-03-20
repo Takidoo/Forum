c70e2e30dc6778492497335dace674184bbd5665
Taki Do <ferrusi.enzo06@gmail.com>
2025-03-20 11:46:10 +0100

    API login

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"Forum/API"
 	"Forum/Database"
+	"Forum/PageHandlers"
 	"net/http"
 )
 
 func main() {
 	print("test")
+	Database.RegisterUser("Takido", "test")
 	Database.ConnectDB()
+	http.HandleFunc("/", PageHandlers.LoginPage)
 	http.HandleFunc("/login", API.Login)
 	http.ListenAndServe(":80", nil)
 }
