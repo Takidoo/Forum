f977132d00db8ce7d06481790a2e57d7b44b0c9d
laeti91 <la.exclupro@gmail.com>
2025-03-21 10:35:21 +0100

    changement nom html, modif lien handler, ajout de css pour la connexion

--- Pagehandlers/LoginPage.go
+++ Pagehandlers/LoginPage.go
@@ -6,6 +6,6 @@ import (
 )
 
 func LoginPage(w http.ResponseWriter, r *http.Request) {
-	var tmpl, _ = template.ParseFiles("WebPages/home.html")
+	var tmpl, _ = template.ParseFiles("WebPages/connexion.html")
 	tmpl.Execute(w, nil)
 }
