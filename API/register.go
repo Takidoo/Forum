718f7e92c10229439ac342a71fec2e290bf019b5
Taki Do <ferrusi.enzo06@gmail.com>
2025-03-24 10:36:19 +0100

    Ajout du systeme de création d'un user

--- API/register.go
+++ API/register.go
@@ -2,32 +2,44 @@ package API
 
 import (
 	"Forum/Database"
-	"fmt"
+	"encoding/json"
+	"net/http"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
-func RegisterUser(username, password string) error {
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if username == "" || password == "" {
+		http.Error(w, "Invalid Agrs", http.StatusBadRequest)
+	}
+	w.Header().Set("Content-Type", "application/json")
 	var count int
 	checkQuery := `SELECT COUNT(1) FROM users WHERE username = ?;`
 	err := Database.DB.QueryRow(checkQuery, username).Scan(&count)
 	if err != nil {
-		return fmt.Errorf("Erreur lors de la vérification de l'existence de l'utilisateur : %v", err)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Can't verify if user exist"})
+		return
 	}
 
 	if count > 0 {
-		return fmt.Errorf("Nom d'utilisateur déjà pris")
+		json.NewEncoder(w).Encode(map[string]string{"message": "Username taken"})
+		return
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return fmt.Errorf("Erreur lors du hachage du mot de passe : %v", err)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Can't create user"})
+		http.Error(w, "Can't create user", http.StatusInternalServerError)
+		return
 	}
 	query := `INSERT INTO users (username, password) VALUES (?, ?)`
 	_, err = Database.DB.Exec(query, username, string(hashedPassword))
 	if err != nil {
-		return fmt.Errorf("Erreur lors de l'insertion du nouvel utilisateur : %v", err)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Can't create user"})
+		http.Error(w, "Can't create user", http.StatusInternalServerError)
+		return
 	}
-
-	return nil
+	json.NewEncoder(w).Encode(map[string]string{"message": "Success"})
 }
