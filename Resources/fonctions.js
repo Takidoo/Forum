fa370ccb1d745b07934990023ad70a5a3831a9f8
Taki Do <ferrusi.enzo06@gmail.com>
2025-03-27 09:39:20 +0100

    Login page update

---
+++ Resources/fonctions.js
@@ -0,0 +1,119 @@
+// on attend que le contenu DOM de la page soit chargé
+document.addEventListener("DOMContentLoaded", function() { //on ajoute une écoute d'événement au chargement de la page
+    console.log("DOM chargé"); //affiche un message console pour vérifie que le contenu est bien chargé
+    
+    document.getElementById("initialButtons").style.display="flex"; //on force l'affichage des boutons initiaux
+    /* //appel de la fonction qui créer le bouton retour 
+    addBackButton(); //on ajoute le bouton retour à chaque formulaire */
+
+    //on ajoute une écoute d'événement au clic sur le bouton Login
+    document.getElementById("goToConnexion").addEventListener("click", function(e) {
+        e.preventDefault(); //empêche le rechargement de la page
+        console.log("click sur le bouton login"); //on affiche un message console pour vérifier que le clic fonctionne
+        document.getElementById("initialButtons").style.display="none"; //cache les boutons initiaux à l'activation du bouton connexion
+        document.getElementById("connexion").style.display="flex"; //rend la div connexion visible à l'activation du bouton connexion
+    });
+
+    //on ajoute une écoute d'événement au clic sur le bouton Register
+    document.getElementById("goToInscription").addEventListener("click", function(e) {
+        e.preventDefault(); //empêche le rechargement de la page
+        console.log("click sur le bouton register"); //on affiche un message console pour vérifier que le clic fonctionne
+        document.getElementById("initialButtons").style.display="none"; //rend les boutons invisibles à l'activation du bouton inscription
+        document.getElementById("logo").style.display="none"; //rend le logo invisible
+        document.getElementById("profilPicture").style.display="block"; //rend la div d'insertion de photo de profil visible
+        document.getElementById("inscription").style.display="flex"; //rend la div inscription visible
+    });
+
+    //prévisualisation de l'image de profil
+    document.getElementById("picture").addEventListener("change", function(event) { //affiche l'image sélectionnée dans la div preview à partir de l'input file
+        const preview = document.getElementById("preview"); //recupère la div preview avec un id unique
+        const file = event.target.files[0]; //recupère le fichier sélectionné à l'indice 1 dans le tableau files
+    
+        if (file) { //si un fichier est sélectionné
+            const reader = new FileReader(); //on crée un objet FileReader pour lire le contenu du fichier que l'on stocke dans la variable reader
+            reader.onload = function(e) { //lorsque le fichier est chargé
+                preview.src = e.target.result; //affiche l'image sélectionnée
+                preview.style.display = "block"; //rend l'image visible
+            };
+            reader.readAsDataURL(file); //lit le contenu du fichier
+        }
+    });
+
+    //ajout de bouton back dans les div connexion et inscription
+    //selection des conteneurs des formulaires de connexion et d'inscription
+    const connexionDiv = document.getElementById("connexion"); //recupère le formulaire de connexion
+    const inscriptionDiv = document.getElementById("inscription"); //recupère le formulaire d'inscription
+
+    if (connexionDiv) { //si le formulaire de connexion existe
+        console.log("formulaire de connexion trouvé, ajout du bouton Back"); //on affiche un message console
+        
+        //création de bouton retour au clic sur le bouton Submit du formulaire de connexion pour vérifier les identifiants et le rediriger vers la page d'accueil
+        const backButtonConnexion = document.createElement("button"); //crée un bouton
+        backButtonConnexion.textContent = "Back"; //ajoute le texte "Back" au bouton
+        backButtonConnexion.type = "button"; //ajoute le type button au bouton
+        backButton.classList.add("backButton"); //ajoute la classe backButton au bouton
+
+        //ajoute une écoute d'événement au clic sur le bouton retour
+        backButtonConnexion.addEventListener("click", function(e) {
+            e.preventDefault(); //on empêche le rechargement de la page
+            console.log("click sur le bouton Back connexion"); //on affiche un message console pour vérifier que le clic du bouton retour fonctionne
+            connexionDiv.style.display="none"; //on rend le formulaire invisible
+            document.getElementById("initialButtons").style.display="flex"; //les boutons initiaux visibles
+        });
+
+        //on ajoute le bouton au formulaire de connexion
+        connexionDiv.appendChild(backButtonConnexion) //ajoute le bouton à la div du formulaire de connexion
+        }else{
+            console.error("formulaire de connexion non trouvé"); //on affiche un message d'erreur sur la console si la div du formulaire de connexion n'est pas trouvé
+        }
+
+    if (inscriptionDiv) { //si le formulaire d'inscription existe
+        console.log("formulaire d'inscription trouvé, ajout du bouton Back"); //on affiche un message console
+        
+        //on créer de bouton retour au clic sur le bouton Submit du formulaire d'inscription pour vérifier les identifiants et le rediriger vers la page d'accueil
+        const backButtonInscription = document.createElement("button"); //on crée un bouton
+        backButtonInscription.textContent = "Back"; //on ajoute le texte "Back" au bouton
+        backButtonInscription.type = "button"; //on ajoute le type button au bouton
+        backButton.classList.add("backButton"); //on ajoute la classe backButton au bouton
+
+        //on ajoute une écoute d'événement au clic sur le bouton retour
+        backButtonInscription.addEventListener("click", function(e) {
+            e.preventDefault(); //on empêche le rechargement de la page
+            console.log("click sur le bouton Back connexion"); //on affiche un message console pour vérifier que le clic du bouton retour fonctionne
+            inscriptionDiv.style.display="none"; //on rend le formulaire invisible
+            document.getElementById("initialButtons").style.display="flex"; //les boutons initiaux sont visibles
+        });
+
+        //on ajoute le bouton au formulaire d'inscription
+        inscriptionDiv.appendChild(backButtonInscription) //on ajoute le bouton à la div du formulaire d'inscription
+        }else{
+            console.error("formulaire d'inscription non trouvé"); //on affiche un message d'erreur sur la console si la div du formulaire d'inscription n'est pas trouvé
+        }
+
+    /* //création de boutons retour au clic sur le bouton Submit du formulaire de connexion et d'inscription
+    function addBackButton() {
+    const forms = document.querySelectorAll(".form:not(#initialButtons)"); //recupère tous les formulaires sauf celui des boutons initiaux
+        
+    forms.forEach(form => { //on parcourt chaque formulaire et on vérifie si un bouton retour est déjà présent
+        if (!form.querySelector("backButton")) { //on vérifie si le bouton retour existe déja     
+        const backButton = document.createElement("button"); //crée un bouton
+                backButton.textContent = "Back"; //ajoute le texte "Back" au bouton
+                backButton.classList.add("backButton"); //ajoute la classe backButton au bouton
+
+                backButton.addEventListener("click", function() { //ajoute une écoute d'événement au clic sur le bouton
+                    e.preventDefault(); //on empêche le rechargement de la page
+                    console.log("click sur le bouton retour"); //on affiche un message console pour vérifier que le clic du bouton retour fonctionne
+                    form.style.display="none"; //on rend le formulaire invisible
+                    document.getElementById("initialButtons").style.display="flex"; //les boutons initiaux visibles
+
+                    if (form.id === "inscription") { //si le formulaire est celui d'inscription
+                        document.getElementById("logo").style.display="block"; //on rend le logo visible
+                        document.getElementById("profilPicture").style.display="none"; //on cache la div d'insertion de photo de profil
+                    }
+                });
+
+                form.appendChild(backButton); //ajoute le bouton au formulaire
+        }
+    });
+    }; */
+});
--- main.go
+++ main.go
@@ -10,15 +10,15 @@ import (
 func main() {
 	// Prérequis
 	Database.ConnectDB()
-	http.Handle("/Ressources/", http.StripPrefix("/Ressources/", http.FileServer(http.Dir("./Ressources"))))
+	http.Handle("/Resources/", http.StripPrefix("/Resources/", http.FileServer(http.Dir("./Resources"))))
 
 	// API
-	http.HandleFunc("/login", API.Login)
-	http.HandleFunc("/register", API.Register)
-	http.HandleFunc("/FetchThreadPosts", API.FetchThreadPosts)
-	http.HandleFunc("/UserInfo", API.UserInfo)
-	http.HandleFunc("/CreateThread", API.CreateThread)
-	http.HandleFunc("/CreatePost", API.CreatePost)
+	http.HandleFunc("/api/login", API.Login)
+	http.HandleFunc("/api/register", API.Register)
+	http.HandleFunc("/api/FetchThreadPosts", API.FetchThreadPosts)
+	http.HandleFunc("/api/api/UserInfo", API.UserInfo)
+	http.HandleFunc("/api/CreateThread", API.CreateThread)
+	http.HandleFunc("/api/CreatePost", API.CreatePost)
 
 	// Pages
 	http.HandleFunc("/", PageHandlers.LoginPage)
