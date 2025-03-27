92e5690b72928611beec259953d3969f94a005c1
Taki Do <ferrusi.enzo06@gmail.com>
2025-03-27 09:42:11 +0100

    Login page update

---
+++ PageHandlers/WebPageHandlers.go
@@ -0,0 +1,16 @@
+package PageHandlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func TestPageHandler(w http.ResponseWriter, r *http.Request) {
+	var tmpl, _ = template.ParseFiles("WebPages/home.html")
+	tmpl.Execute(w, nil)
+}
+
+func LoginPageHandler(w http.ResponseWriter, r *http.Request) {
+	var tmpl, _ = template.ParseFiles("WebPages/login.html")
+	tmpl.Execute(w, nil)
+}
