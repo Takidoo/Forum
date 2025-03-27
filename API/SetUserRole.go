877e4426962433d31697d6de73a8d1bbc5a76d81
Taki Do <ferrusi.enzo06@gmail.com>
2025-03-27 11:59:11 +0100

    Implémentation du système de modération

--- API/SetUserRole.go
+++ API/SetUserRole.go
@@ -7,10 +7,15 @@ import (
 )
 
 func SetUserRole(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.Error(w, "Méthode invalide", http.StatusMethodNotAllowed)
+		return
+	}
 	if !Database.UserIsAdmin(w, r) {
 		http.Error(w, "You're not admin", http.StatusUnauthorized)
 		return
 	}
+
 	_, err := Database.DB.Exec("UPDATE users SET role=? WHERE id=?", r.FormValue("RoleID"), r.FormValue("UserID"))
 	if err != nil {
 		json.NewEncoder(w).Encode(map[string]string{"error": "Can't set role to user"})
