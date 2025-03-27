8c542ee43ceb276cab748c3ae0fa2a5784a2f3d7
Taki Do <ferrusi.enzo06@gmail.com>
2025-03-27 11:39:35 +0100

    Implémentation du système de modération

---
+++ API/SetUserRole.go
@@ -0,0 +1,19 @@
+package API
+
+import (
+	"Forum/Database"
+	"encoding/json"
+	"net/http"
+)
+
+func SetUserRole(w http.ResponseWriter, r *http.Request) {
+	if !Database.UserIsAdmin(w, r) {
+		http.Error(w, "You're not admin", http.StatusUnauthorized)
+		return
+	}
+	_, err := Database.DB.Exec("UPDATE users SET role WHERE id=?", r.FormValue("RoleID"))
+	if err != nil {
+		json.NewEncoder(w).Encode(map[string]string{"error": "Can't set role to user"})
+	}
+	json.NewEncoder(w).Encode(map[string]string{"success": "Role set to user"})
+}
--- PageHandlers/WebPageHandlers.go
+++ PageHandlers/WebPageHandlers.go
@@ -9,6 +9,10 @@ func TestPageHandler(w http.ResponseWriter, r *http.Request) {
 	var tmpl, _ = template.ParseFiles("WebPages/home.html")
 	tmpl.Execute(w, nil)
 }
+func AdminPageHandler(w http.ResponseWriter, r *http.Request) {
+	var tmpl, _ = template.ParseFiles("WebPages/admin.html")
+	tmpl.Execute(w, nil)
+}
 
 func LoginPageHandler(w http.ResponseWriter, r *http.Request) {
 	var tmpl, _ = template.ParseFiles("WebPages/login.html")
