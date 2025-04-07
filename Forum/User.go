3491eb9cdefee321fb4b6fe2702b58298e8d37db
Taki Do <ferrusi.enzo06@gmail.com>
2025-04-07 10:18:53 +0200

    Amélioration de la page admin

--- Forum/User.go
+++ Forum/User.go
@@ -17,7 +17,7 @@ func GetUser(session string) (User, error) {
 	err := Database.DB.QueryRow("SELECT user_id FROM sessions WHERE token = ?", session).Scan(&user.ID)
 	if err != nil {
 
-		return User{}, fmt.Errorf("Cannot fetch user infos")
+		return User{}, fmt.Errorf("cannot fetch user infos")
 	}
 	_ = Database.DB.QueryRow("SELECT username, role FROM users WHERE id = ?", user.ID).Scan(&user.Username, &user.Role)
 	return user, nil
